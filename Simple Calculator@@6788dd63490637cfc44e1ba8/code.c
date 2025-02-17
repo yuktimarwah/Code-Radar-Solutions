3ad44dbaa929ba025f0edcaa16d1b1dfc13b7596
yukti marwah <yuktimarwahgithub@gmail.com>
2025-02-18 03:32:17 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,7 +14,7 @@ int main(){
        printf("%d",a*b);
         break;
         case'/':
-       printf("%d",a/b);
+       printf("%d",a+b);
         break;
         default:
         printf("error");
