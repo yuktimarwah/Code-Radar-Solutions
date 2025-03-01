cc74adb54ad3fb54b6a5e4879551bb85e6eeff20
yukti marwah <yuktimarwahgithub@gmail.com>
2025-03-02 00:48:43 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -4,7 +4,7 @@
         if(n==0||n==1){
             return 0;
         }
-        for(int i=2;i<n/2;i++){
+        for(int i=2;i<n;i++){
             if(n%i==0){
             return 0;
             }
