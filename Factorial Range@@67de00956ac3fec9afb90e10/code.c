2fd5b0bfd4284c8e1c6eead8daab2efcaa04b154
yukti marwah <yuktimarwahgithub@gmail.com>
2025-03-31 18:37:52 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,12 @@
+int factorialRange(int a,int b){
+    for(int i=a;i<=b;i++){
+        if(i==0||i==1){
+            return 1;
+        }
+        int factnM1 = fact(n-1);
+        int c = fact(n-1)*n;
+        return c;
+
+    }
+
+}
